e498ff54c779878732442ad4354de159fe6cf502
Redah <redah.malick@gmail.com>
2020-04-27 17:30:50 +0200

    Entire script. Runs the training and test set. Outputs a file

--- script/vaccine_nlp.py
+++ script/vaccine_nlp.py
@@ -42,7 +42,6 @@ airline_sentiment.plot(kind='bar')
 '''
 
 
-
 features = vaccine_tweets['safe_text'].values
 labels = vaccine_tweets['label'].values
 
@@ -92,6 +91,51 @@ print(accuracy_score(y_test, predictions))
 
 
 
+'''
+This section
+'''
+#Import the test set
+vac_tweets = pd.read_csv("Test.csv")
+
+#This is a df for the results 
+y_out=vac_tweets
+
+
+x_features = vac_tweets['safe_text'].values
+ 
+v_processed_features = []
+
+
+for sentence in range(0, len(x_features)):
+    # Remove all the special characters
+    processed_feature = re.sub(r'\W', ' ', str(x_features[sentence]))
+
+    # remove all single characters
+    processed_feature= re.sub(r'\s+[a-zA-Z]\s+', ' ', processed_feature)
+
+    # Remove single characters from the start
+    processed_feature = re.sub(r'\^[a-zA-Z]\s+', ' ', processed_feature) 
+
+    # Substituting multiple spaces with single space
+    processed_feature = re.sub(r'\s+', ' ', processed_feature, flags=re.I)
+
+    # Removing prefixed 'b'
+    processed_feature = re.sub(r'^b\s+', '', processed_feature)
+
+    # Converting to Lowercase
+    processed_feature = processed_feature.lower()
+
+    v_processed_features.append(processed_feature)
+
+v_processed_features = vectorizer.transform(v_processed_features).toarray()    
+
+
+#do this after cleaning
+results = text_classifier.predict(v_processed_features)
+
+y_out['label']=results
+y_out=y_out.drop(['safe_text'],axis=1)
+y_out.to_csv('results.csv',index=False)
 
 
 
